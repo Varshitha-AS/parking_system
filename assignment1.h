a14616e5a2d29a0cd2cc4ef2276c1349af218d35
Varshitha A S <varshithasomashekar02@gmail.com>
2024-06-25 08:51:00 +0530

    assignment

---
+++ assignment1.h
@@ -0,0 +1,13 @@
+#ifndef PARKING_SLOT_H_INCLUDED
+#define PARKING_SLOT_H_INCLUDED
+
+#include<stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> 
+
+int findNearestVacantSlot()
+void parkVehicle()
+void unparkVehicle()
+void displayParkingLot()
+
+#endif
